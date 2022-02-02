28c256f4ab7be4f9d95a3bf29991fbbfd9b5853b
Devan Becker <d7becker@gmail.com>
2022-02-01 20:39:06 -0500

    Got to support vector machines

--- QuantQual-figs.R
+++ QuantQual-figs.R
@@ -8,7 +8,7 @@ library(here)           # Finding files
 library(palmerpenguins) # Data
 library(knitr)          # Pretty tables (kable() function)
 library(broom)          # better linear model output
-
+library(e1071)
 
 
 
@@ -160,3 +160,77 @@ gg_pat1 + gg_pat2 + plot_layout(guides = "collect")
 dev.off()
 
 
+# Classification: Binary Target
+pengtoo <- filter(penguins, !is.na(sex), species == "Gentoo")
+pengtoo %>% slice(1:7) %>% 
+    kable(format = "pipe")
+
+# Classification: Choosing between two options
+cutoff <- 198
+png(here("figs", "7-SVM.png"), height = 300, width = 600)
+ggplot(penguins, 
+        aes(x = flipper_length_mm, y = sex, colour = sex)) +
+    geom_jitter() +
+    scale_colour_manual(values = c("forestgreen", "darkorchid")) +
+    labs(x = "Flipper Length (mm)", y = "Sex",
+        colour = "Sex", 
+        title = "Which value of Flipper Length best splits Biosex?") + 
+    annotate("segment", x = cutoff, xend = cutoff, y = -Inf, yend = Inf) +
+    annotate("segment", x = cutoff, y = 1, yend = 1, 
+        xend = max(penguins$flipper_length_mm[penguins$sex == "female"],
+            na.rm = TRUE),  
+        colour = "forestgreen", arrow = arrow()) +
+    annotate("segment", x = cutoff, y = 1, yend = 1, 
+        xend = min(penguins$flipper_length_mm[penguins$sex == "female"],
+            na.rm = TRUE),  
+        colour = "forestgreen", arrow = arrow()) +
+    annotate("segment", x = cutoff, y = 2, yend = 2,
+        xend = max(penguins$flipper_length_mm[penguins$sex == "male"],
+            na.rm = TRUE), 
+        colour = "darkorchid", arrow = arrow()) +
+    annotate("segment", x = cutoff, y = 2, yend = 2,
+        xend = min(penguins$flipper_length_mm[penguins$sex == "male"],
+            na.rm = TRUE),  
+        colour = "darkorchid", arrow = arrow()) +
+    annotate("text", x = cutoff, y = 1, hjust = 1.2, vjust = 1.2,
+        label = sum(penguins$flipper_length_mm >= cutoff & penguins$sex == "male"), size = 10) +
+    annotate("text", x = cutoff, y = 1, hjust = -0.2, vjust = 1.2,
+        label = sum(penguins$flipper_length_mm < cutoff & penguins$sex == "male"), size = 10) +
+    annotate("text", x = cutoff, y = 2, hjust = -0.2, vjust = 1.2,
+        label = sum(penguins$flipper_length_mm < cutoff & penguins$sex == "female"), size = 10) +
+    annotate("text", x = cutoff, y = 2, hjust = 1.2, vjust = 1.2,
+        label = sum(penguins$flipper_length_mm >= cutoff & penguins$sex == "female"), size = 10)
+dev.off()
+
+
+# Classification: More dimensions!
+svm_flip <- svm(factor(sex) ~ flipper_length_mm + body_mass_g, 
+    data = pengtoo, scale = FALSE, kernel = "linear")
+x <- as.matrix(pengtoo[, c("flipper_length_mm", "body_mass_g")])
+beta <- drop(t(svm_flip$coefs) %*% x[svm_flip$index,])
+beta0 <- svm_flip$rho
+intercept <- beta0 / beta[2]
+slope <- -beta[1] / beta[2]
+
+png(here("figs", "8-SVM2.png"), height = 300, width = 600)
+ggplot(pengtoo) +
+    aes(x = flipper_length_mm, y = body_mass_g, colour = sex) +
+    geom_point() +
+    scale_colour_manual(values = c("forestgreen", "darkorchid")) +
+    labs(x = "Flipper Length (mm)", y = "Body Mass (g)",
+        title = "Above the line = male, below = female",
+        colour = "Sex") +
+    geom_abline(intercept = intercept, slope = slope)
+dev.off()
+
+
+png(here("figs", "9-SVM3.png"), height = 300, width = 600)
+ggplot(penguins) +
+    aes(x = flipper_length_mm, y = body_mass_g, colour = sex) +
+    geom_point() +
+    scale_colour_manual(values = c("forestgreen", "darkorchid")) +
+    labs(x = "Flipper Length (mm)", y = "Body Mass (g)",
+        title = "Above the line = male, below = female",
+        colour = "Sex") +
+    geom_abline(intercept = intercept, slope = slope)
+dev.off()
